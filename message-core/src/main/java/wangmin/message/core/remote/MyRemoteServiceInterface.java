2869d9896b0458fc9709cc8b22e6e7a9a8112585
wm3418925 <591593903@qq.com>
2016-12-21 23:22:56 +0800

    整理消息 消费者 (消息中间件添加之前)

--- message-core/src/main/java/wangmin/message/core/remote/MyRemoteServiceInterface.java
+++
@@ -1,7 +0,0 @@
-package wangmin.message.core.remote;
-
-import wangmin.message.core.entity.MyUserEntity;
-
-public interface MyRemoteServiceInterface {
-	MyUserEntity getUserById(String id);
-}
--- unconsumed-message-checker/src/test/java/lanchuang/rdms/test_service/Test.java
+++ unconsumed-message-checker/src/test/java/lanchuang/rdms/test_service/Test.java
@@ -1,4 +1,4 @@
-package wangmin.message.test_service;
+package lanchuang.rdms.test_service;
 
 import java.io.InputStream;
 import java.util.Properties;
@@ -9,8 +9,7 @@ import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import wangmin.message.core.entity.MyUserEntity;
-import wangmin.message.core.remote.MyRemoteServiceInterface;
+import wangmin.message.core.remote.MessageServiceInterface;
 
 @WebListener
 public class Test {
@@ -20,8 +19,8 @@ public class Test {
 
 	
 	public void test() {
-		MyRemoteServiceInterface service = context.getBean(MyRemoteServiceInterface.class);
-		MyUserEntity result = service.getUserById("1");
+		MessageServiceInterface service = context.getBean(MessageServiceInterface.class);
+		boolean result = service.closeMessage("1");
 		System.out.println("result = " + result);
 	}
 	
